36826b46bf25d3fa54863fe8845f7e95eaa759f5
Manuel Kasper <mk@neon1.net>
2010-05-06 20:05:54 +0000

    * prepare for new release

--- www/top.php
+++ www/top.php
@@ -90,7 +90,7 @@ foreach ($knownlinks as $link) {
 </div>
 
 <div id="footer">
-AS-Stats v1.3 written by Manuel Kasper, Monzoon Networks AG.<br/>
+AS-Stats v1.31 written by Manuel Kasper, Monzoon Networks AG.<br/>
 <?php if ($outispositive): ?>
 Outbound traffic: positive / Inbound traffic: negative
 <?php else: ?>
