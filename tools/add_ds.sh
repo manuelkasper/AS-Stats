06cafdafc0a78b9ef6412898944d8f0cd0b055d0
Manuel Kasper <mk@neon1.net>
2010-06-14 11:33:53 +0000

    * fix for add_ds.sh to support new directory structure

--- tools/add_ds.sh
+++ tools/add_ds.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-for f in *.rrd; do
-	echo "file: $f"
-	mv $f $f.old
-	rrdtool dump $f.old | /path/to/add_ds_proc.pl | rrdtool restore - $f.new
-	mv $f.new $f
+A=`ls -1`
+
+for i in $A ; do
+	echo ""
+	echo "dir: $i"
+	echo ""
+	cd $i
+
+	for f in *.rrd; do
+		echo "file: $f"
+		mv $f $f.old
+		rrdtool dump $f.old | /path/to/add_ds_proc.pl | rrdtool restore - $f.new
+		mv $f.new $f
+	done
+
+	cd ../
 done
