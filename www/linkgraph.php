1856c64f3d9188f6a69ed39e7cee2e850c29bb78
Manuel Kasper <mk@neon1.net>
2013-12-06 09:09:56 +0100

    Fix security issue

--- www/linkgraph.php
+++ www/linkgraph.php
@@ -71,7 +71,7 @@ $cmd = "$rrdtool graph - " .
 	"--slope-mode --alt-autoscale -u 0 -l 0 --imgformat=PNG --base=1000 --height=$height --width=$width " .
 	"--color BACK#ffffff00 --color SHADEA#ffffff00 --color SHADEB#ffffff00 ";
 
-if (@$_GET['v'])
+if (isset($_GET['v']) && is_numeric($_GET['v']))
 	$cmd .= "--title IPv" . $_GET['v'] . " ";
 
 /* geneate RRD DEFs */
