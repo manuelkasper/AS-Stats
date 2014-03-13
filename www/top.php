557036deb81e60627ae81593cc69548774256699
Nikolay Shopik <shopik@inblock.ru>
2014-03-13 16:03:32 +0300

    fix graphs size downscaling

--- www/top.php
+++ www/top.php
@@ -89,8 +89,8 @@ echo join(" | ", $htmllinks);
 	</th>
 	<td>
 		<?php if ($showv6): ?>
-		<a href="history.php?v=4&amp;as=<?php echo $as; ?>" target="_blank"><img alt="AS graph" src="gengraph.php?as=<?php echo $as; ?>&amp;width=500&amp;height=150&amp;v=4&amp;nolegend=1&amp;dname=<?php echo rawurlencode("AS" . $as . " - " . $asinfo['descr'] . " - IPV4"); ?>&amp;start=<?=$start?>&amp;end=<?=$end?>" width="581" height="207" border="0" /></a>
-		<a href="history.php?v=6&amp;as=<?php echo $as; ?>" target="_blank"><img alt="AS graph" src="gengraph.php?as=<?php echo $as; ?>&amp;width=500&amp;height=150&amp;v=6&amp;nolegend=1&amp;dname=<?php echo rawurlencode("AS" . $as . " - " . $asinfo['descr'] . " - IPV6"); ?>&amp;start=<?=$start?>&amp;end=<?=$end?>" width="581" height="207" border="0" /></a>
+		<a href="history.php?v=4&amp;as=<?php echo $as; ?>" target="_blank"><img alt="AS graph" src="gengraph.php?as=<?php echo $as; ?>&amp;width=500&amp;height=150&amp;v=4&amp;nolegend=1&amp;dname=<?php echo rawurlencode("AS" . $as . " - " . $asinfo['descr'] . " - IPV4"); ?>&amp;start=<?=$start?>&amp;end=<?=$end?>" width="597" height="207" border="0" /></a>
+		<a href="history.php?v=6&amp;as=<?php echo $as; ?>" target="_blank"><img alt="AS graph" src="gengraph.php?as=<?php echo $as; ?>&amp;width=500&amp;height=150&amp;v=6&amp;nolegend=1&amp;dname=<?php echo rawurlencode("AS" . $as . " - " . $asinfo['descr'] . " - IPV6"); ?>&amp;start=<?=$start?>&amp;end=<?=$end?>" width="597" height="207" border="0" /></a>
 		<?php else: ?>
 		<a href="history.php?as=<?php echo $as; ?>" target="_blank"><img alt="AS graph" src="gengraph.php?as=<?php echo $as; ?>&amp;width=500&amp;height=150&amp;nolegend=1&amp;dname=<?php echo rawurlencode("AS" . $as . " - " . $asinfo['descr'] . ""); ?>&amp;start=<?=$start?>&amp;end=<?=$end?>" width="581" height="189" border="0" /></a>
 		<?php endif; ?>
